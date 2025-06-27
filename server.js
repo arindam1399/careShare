b2bf0ff91103cd39b7c76bd1f3674bf051c3480c
arindam1399 <kingarindam1999@gmail.com>
2025-06-27 15:00:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT,() =>{
 console.log(`Listening on port ${PORT}`);
 });
-
-app.get('/', (req, res) => {
-    res.send('Welcome to CareShare! Backend is working ');
-});
